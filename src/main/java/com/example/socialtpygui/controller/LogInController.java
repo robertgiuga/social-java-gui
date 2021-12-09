a118694758c6e54b2593f0929365bea210889eed
Robert Giuga <grir2934@scs.ubbcluj.ro>
2021-12-09 13:11:49 +0200

    MainWindow added and show it from login button created

--- src/main/java/com/example/socialtpygui/controller/LogInController.java
+++ src/main/java/com/example/socialtpygui/controller/LogInController.java
@@ -1,8 +1,15 @@
 package com.example.socialtpygui.controller;
 
+import com.example.socialtpygui.RunApplication;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LogInController {
 
@@ -11,11 +18,21 @@ public class LogInController {
     @FXML
     private TextField passswordTxt;
     @FXML
-    private Button loginBtn;
+    private Button logInBtn;
 
     @FXML
-    private void handlerLogIn(){
-        usernameTxt.setText("buton apasat");
+    private void handlerLogIn() throws IOException {
+        //verify if ok login
+
+        FXMLLoader fxmlLoader = new FXMLLoader(RunApplication.class.getResource("mainWindow.fxml"));
+        Stage manWindowStage= new Stage();
+        Scene scene = new Scene(fxmlLoader.load(), 377, 210);
+        manWindowStage.setScene(scene);
+        manWindowStage.initModality(Modality.NONE);
+        manWindowStage.show();
+
+        Stage logInStage =(Stage) logInBtn.getScene().getWindow();
+        logInStage.close();
     }
 
 }
