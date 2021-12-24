0da512ff69184c3e574a5fde2d5cb42de3e87df2
Robert Giuga <grir2934@scs.ubbcluj.ro>
2021-12-24 17:35:25 +0200

    minor ConvController refactorin

--- src/main/java/com/example/socialtpygui/controller/ShowConvController.java
+++ src/main/java/com/example/socialtpygui/controller/ShowConvController.java
@@ -25,6 +25,7 @@ import javafx.scene.layout.Pane;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ShowConvController implements Observer<NewMessageEvent> {
@@ -62,54 +63,19 @@ public class ShowConvController implements Observer<NewMessageEvent> {
     private int lastConvMsjId;
 
     /**
-     * send a message to user from conversation
-     * @throws IOException
+     * send a message witch is create by the logged user in the GUI
+     * and adds the new message in the GUI
+     * @throws IOException .
      */
     private void sendMessage() throws IOException {
         scrollPaneShowConv.setVvalue(1.0f);
-        List<String> to = new ArrayList<>();
-        to.add(email2User);
-        Pane item = null;
+        Pane item;
+
         if(groupId == null) {
-            if (dragMessage == null) {
-                MessageDTO messageDTO = service.sendMessage(new MessageDTO(loggedUser.getId(), to, messageText.getText(), LocalDate.now()));
-                item = createItem(new ReplyMessage(messageDTO, null));
-                item.getChildren().forEach(node -> {
-                    if (node instanceof Label)
-                        node.setId(String.valueOf(messageText.getText()));
-                });
-                lastConvMsjId =messageDTO.getId();
-            }
-            else {
-                ReplyMessage replyMessage = service.replyMessage(new ReplyMessageDTO(new MessageDTO(loggedUser.getId(), to, messageText.getText(), LocalDate.now()), dragMessage.getMessage().getId().toString()));
-                item = createItem(replyMessage);
-                item.getChildren().forEach(node -> {
-                    if (node instanceof Label)
-                        node.setId(String.valueOf(messageText.getText()));
-                });
-                dragMessage = null;
-                lastConvMsjId = replyMessage.getId();
-            }
+            item=sendConvMessage();
         }
         else{
-            if(dragMessage == null) {
-                MessageDTO messageDTO = service.replyAll(new MessageDTO(loggedUser.getId(), null, messageText.getText(), LocalDate.now()), groupId);
-                item = createGroupItem(new ReplyMessage(messageDTO, null));
-                item.getChildren().forEach(node -> {
-                    if (node instanceof Label)
-                        node.setId(String.valueOf(messageText.getText()));
-                });
-                lastGroupMsjId =messageDTO.getId();
-            }
-            else{
-                ReplyMessage replyMessage = service.replyMessageGroup(new ReplyMessageDTO(new MessageDTO(loggedUser.getId(), null, messageText.getText(), LocalDate.now()), dragMessage.getMessage().getId().toString()), groupId);
-                item = createGroupItem(replyMessage);
-                item.getChildren().forEach(node -> {
-                    if (node instanceof Label)
-                        node.setId(String.valueOf(messageText.getText()));
-                });
-                lastGroupMsjId = replyMessage.getId();
-            }
+            item= sendGroupMessage();
         }
 
         gridShowMessages.add(item, 1, gridShowMessages.getRowCount());
@@ -121,9 +87,61 @@ public class ShowConvController implements Observer<NewMessageEvent> {
     }
 
     /**
+     * send a private message to the User witch is in the conv displayed (email2User)
+     * @return the pane with the new message
+     * @throws IOException .
+     */
+    private Pane sendConvMessage() throws IOException {
+        Pane item;
+        ReplyMessage message;
+        if (dragMessage == null) {
+            MessageDTO messageDTO = service.sendMessage(new MessageDTO(loggedUser.getId(), List.of(email2User), messageText.getText(), LocalDate.now()));
+            message= new ReplyMessage(messageDTO,null);
+            lastConvMsjId =messageDTO.getId();
+        }
+        else {
+            message = service.replyMessage(new ReplyMessageDTO(new MessageDTO(loggedUser.getId(), List.of(email2User), messageText.getText(), LocalDate.now()), dragMessage.getMessage().getId().toString()));
+            dragMessage = null;
+            lastConvMsjId = message.getId();
+        }
+        item = createItem(message);
+        item.getChildren().forEach(node -> {
+            if (node instanceof Label)
+                node.setId(String.valueOf(messageText.getText()));
+        });
+        return item;
+    }
+
+    /**
+     * sends a message to the group witch is displayed(groupID)
+     * @return the Pane with the new message
+     * @throws IOException .
+     */
+    private Pane sendGroupMessage() throws IOException {
+        Pane item;
+        ReplyMessage message;
+        if(dragMessage == null) {
+            MessageDTO messageDTO = service.replyAll(new MessageDTO(loggedUser.getId(), null, messageText.getText(), LocalDate.now()), groupId);
+            message= new ReplyMessage(messageDTO,null);
+            lastGroupMsjId =messageDTO.getId();
+        }
+        else{
+            message = service.replyMessageGroup(new ReplyMessageDTO(new MessageDTO(loggedUser.getId(), null, messageText.getText(), LocalDate.now()), dragMessage.getMessage().getId().toString()), groupId);
+            lastGroupMsjId = message.getId();
+            dragMessage=null;
+        }
+        item = createGroupItem(message);
+        item.getChildren().forEach(node -> {
+            if (node instanceof Label)
+                node.setId(String.valueOf(messageText.getText()));
+        });
+        return item;
+    }
+
+    /**
      * create a new view of a Message
      * @return a Pane
-     * @throws IOException
+     * @throws IOException .
      */
     private Pane createItem(ReplyMessage replyMessage) throws IOException{
         FXMLLoader loader = new FXMLLoader(LogInApplication.class.getResource("showConv-item.fxml"));
@@ -134,10 +152,10 @@ public class ShowConvController implements Observer<NewMessageEvent> {
     }
 
     /**
-     *
-     * @param replyMessage
-     * @return
-     * @throws IOException
+     * create a group item
+     * @param replyMessage the message witch is going to be displayed
+     * @return the Pane with the item
+     * @throws IOException .
      */
     private Pane createGroupItem(ReplyMessage replyMessage) throws IOException {
         FXMLLoader loader = new FXMLLoader(LogInApplication.class.getResource("showConv-item.fxml"));
@@ -151,9 +169,9 @@ public class ShowConvController implements Observer<NewMessageEvent> {
     /**
      * initialize loggedUser, service and email
      * load into gridPane conversation between two users
-     * @param service
-     * @param loggedUser
-     * @param email
+     * @param service .
+     * @param loggedUser .
+     * @param email .
      */
     public void load(SuperService service, User loggedUser, String email){
         service.addObserver(this);
@@ -225,19 +243,19 @@ public class ShowConvController implements Observer<NewMessageEvent> {
 
     /**
      * handle press ok 'ENTER' key sending a Message with text from textField
-     * @throws IOException
+     * @throws IOException .
      */
     @FXML
     private void pressedSendButton() throws IOException {
         if(!messageText.getText().equals("")){
             sendMessage();
-            }
+        }
     }
 
     /**
      * handle press Send Button sending a message in current conversation
-     * @param event
-     * @throws IOException
+     * @param event .
+     * @throws IOException .
      */
     @FXML
     private void handlerKeyPressed(KeyEvent event) throws IOException {
@@ -249,7 +267,7 @@ public class ShowConvController implements Observer<NewMessageEvent> {
 
     /**
      * set text in label messageToReply as message text from dragged message from conversation
-     * @param m
+     * @param m .
      */
     private void DragMessageHandler(DragMessage m) {
         if(m != null) {
@@ -260,7 +278,7 @@ public class ShowConvController implements Observer<NewMessageEvent> {
 
     /**
      * create a groupSettings view
-     * @throws IOException
+     * @throws IOException .
      */
     @FXML
     private void handlerSettignsButton() throws IOException {
@@ -273,13 +291,16 @@ public class ShowConvController implements Observer<NewMessageEvent> {
         scrollPaneShowConv.setContent(item);
     }
 
+    /**
+     * gets Utils.events for loading new messages
+     * one for private conv and one for group
+     * @param newMessageEvent the event witch is sent
+     */
     @Override
     public void update(NewMessageEvent newMessageEvent) {
         scrollPaneShowConv.setVvalue(1.0f);
         System.out.println("update");
         if(newMessageEvent.getEventType().equals(ChangeEventType.NEW_MSJ)) {
-            //TODO
-            // get msj from a certain id ahead
             service.getConvMessagesGreaterThan(loggedUser.getId(), email2User,lastConvMsjId ).forEach(replyMessage -> {
                 try {
                     Pane item = createItem(replyMessage);
@@ -298,8 +319,6 @@ public class ShowConvController implements Observer<NewMessageEvent> {
             });
         }
         else if(groupId!=null&&newMessageEvent.getEventType().equals(ChangeEventType.NEW_GROUP_MSJ)){
-            //TODO
-           // System.out.println(groupId);
             System.out.println("reload msj");
             service.getGroupMessagesGreaterThen(groupId, lastGroupMsjId).forEach(replyMessage -> {
                 try{
