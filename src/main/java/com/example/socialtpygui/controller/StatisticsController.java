fc371007c8dc475453b9f0866e6762ef791c9101
Robert Giuga <grir2934@scs.ubbcluj.ro>
2021-12-24 16:07:59 +0200

    Mange to get the lib going

--- src/main/java/com/example/socialtpygui/controller/StatisticsController.java
+++ src/main/java/com/example/socialtpygui/controller/StatisticsController.java
@@ -1,12 +1,12 @@
 package com.example.socialtpygui.controller;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
 
 public class StatisticsController {
 
 
 
     public void handlerRaport1Btn() {
-
-
+        PDDocument doc = new PDDocument();
     }
 }
